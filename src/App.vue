1a1062893259c80e590540c0f21e8c9a12dd49c1
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-04-17 22:00:22 +0530

    Board component, Grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="container">
+    <board />
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Board from "./components/Board.vue";
+
+export default {
+  components: {
+    Board,
+  },
+  setup() {},
+};
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background: black;
+}
+.container {
+  height: 90vh;
+  width: 100%;
+  display: grid;
+  place-items: center;
 }
 </style>
---
+++ src/components/Board.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="grid">
+        <div v-for="n in 9" :key="n" class="square">{{n}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+.grid{
+    max-width: 600px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+}
+.square{
+    width: 6em;
+    height: 6em;
+    background-color: #eee;
+
+    display: grid;
+    place-items: center;
+}
+.square:nth-child(odd){
+    background-color: #ccc;
+}
+</style>
