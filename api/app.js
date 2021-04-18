f2529f03b0d4ca1a9aa34fa8bf918ada1d36abfe
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-04-18 22:07:35 +0530

    Fix api req.body undefined

--- api/app.js
+++ api/app.js
@@ -1,14 +1,18 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const { mainLogic, checkWinner } = require('./logic')
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.post('/', (req, res) => {
 	console.log(req.params)
 	console.log(req.body)
 	let board = req.body.board
-	// let userSymbol = req.body.userSymbol
-	let winner = checkWinner(board)
-	let move = mainLogic(board)
-	let result = { winner: winner, move: move }
+	// // let userSymbol = req.body.userSymbol
+	// let winner = checkWinner(board)
+	// let move = mainLogic(board)
+	// let result = { winner: winner, move: move }
 	return res.send(JSON.stringify(result))
 	// return res.send('Hello worlds')
 })
